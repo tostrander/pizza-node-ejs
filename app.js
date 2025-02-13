5dddf0fa8f4e2bf6d9f2ae90c501a6316216b2f6
Tina Ostrander <tina.j.ostrander@gmail.com>
2025-02-13 14:42:32 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+//Import Express
+import express from 'express';
+
+//Instantiate an Express application
+const app = express();
+
+//Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+//Set the view engine for our application
+app.set('view engine', 'ejs');
+
+//Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+//Define a port number for our server to listen on
+const PORT = 3000;
+
+//Define an array to store pizza orders
+const orders = [];
+
+//Define a "default" route for our home page
+app.get('/', (req, res) => {
+
+    // Send our home page as a response to the client
+    res.render('home');
+});
+
+//Define an admin route
+app.get('/admin', (req, res) => {
+    res.render('order-summary', { orders });
+});
+
+//Define a "thank you" route
+app.post('/thankyou', (req, res) => {
+
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size
+    };
+
+    // Add the order to our array
+    orders.push(order);
+    console.log(orders);
+
+    // Send our thank you page
+    res.render('thankyou', { order });
+});
+
+//Tell the server to listen on our specified port
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
