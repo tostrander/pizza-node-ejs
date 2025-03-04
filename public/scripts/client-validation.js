333b9431e72839d269c60cc310addbe1b68e716d
Tina Ostrander <tostrander@greenriver.edu>
2025-03-04 14:33:54 -0800

    Finish client-side validation

--- public/scripts/client-validation.js
+++ public/scripts/client-validation.js
@@ -1,5 +1,6 @@
 document.getElementById('pizza-form').onsubmit = () => {
 
+    clearErrors();
     let isValid = true;
 
     // Validate first name
@@ -9,5 +10,47 @@ document.getElementById('pizza-form').onsubmit = () => {
         isValid = false;
     }
     
+    // Validate last name
+    let lname = document.getElementById('lname').value.trim();
+    if (lname === ""){
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+    
+    // Validate email
+    let email = document.getElementById('email').value.trim();
+    if (email === "" || email.indexOf("@") === -1 ||
+        email.indexOf(".") === -1){
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+        
+    // Validate method (pickup or delivery)
+    let methodButtons = document.getElementsByName("method");
+    let count = 0;
+    for(let i=0; i<methodButtons.length; i++) {
+        if(methodButtons[i].checked) {
+            count++;
+        }
+    }
+    if (count === 0) {
+        document.getElementById("err-method").style.display = "block";
+        isValid = false;
+    }
+
+    // Validate size
+    let size = document.getElementById("size").value;
+    if (size === "none") {
+        document.getElementById("err-size").style.display = "block";
+        isValid = false;
+    }
+
     return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i=0; i<errors.length; i++) {
+        errors[i].style.display = "none";
+    }
 }
